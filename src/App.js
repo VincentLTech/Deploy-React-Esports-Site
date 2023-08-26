9eedebdbc03dad58db1108b01add0c616b346d5a
Vincent <electricplasma411@gmail.com>
2023-08-26 15:46:53 -0400

    second

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My first test
+          My second test
         </p>
         <a
           className="App-link"
